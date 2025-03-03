e072b15ecd6dd27cdb8762ceaf755e1ff89357dd
Nazar Frankiv <nazarfrankiv@outlook.com>
2025-03-03 21:22:31 +0200

    Initial commit

---
+++ MauiLabelIssueRepro/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public List<string> TestItems { get; set; } = [];
+    
+    public MainPage()
+    {
+        InitializeComponent();
+
+        for (int i = 0; i < 30; i++)
+        {
+            TestItems.Add("Test Test Test Test Test Test Test Test Test Test Test Test Test Test");
+        }
+        
+        BindingContext = this;
+    }
+}
---
+++ MauiLabelIssueRepro/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        
+// #if IOS || MACCATALYST
+//         builder.ConfigureMauiHandlers(handlers =>
+//         {
+//             handlers.AddHandler<Microsoft.Maui.Controls.CollectionView, Microsoft.Maui.Controls.Handlers.Items2.CollectionViewHandler2>();
+//             handlers.AddHandler<Microsoft.Maui.Controls.CarouselView, Microsoft.Maui.Controls.Handlers.Items2.CarouselViewHandler2>();
+//         });
+// #endif
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
